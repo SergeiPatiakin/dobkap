3010a40458a12d35faaf4a5e7de9b9414fa99748
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-07-26 12:57:18 +0200

    ePorezi XML progress

--- src/eporezi/index.ts
+++ src/eporezi/index.ts
@@ -8,26 +8,52 @@ export interface OpoData {
   filingDeadline: NaiveDate
   jmbg: string
   fullName: string
-  address: string
+  streetAddress: string
   opstinaCode: string
   filerJmbg: string
   phone: string
   email: string
+  realizationMethod: string
   dividendIncomeInfo: DividendIncomeInfo
 }
 
 type XPathSafeString = string
 
+// Convert local XPath tag-name selector to namespaced selector
 const nsTag = (tagName: XPathSafeString) => `*["${tagName}"=local-name()]`
 
 // Guaranteed to return the original object
 const castToArray = <T>(a: T) => a as any as T[]
 
+// Mutates the document
+const setText = (document: Document, localXPath: string[], text: string): void => {
+  const xPath = localXPath.map(x => `/${nsTag(x)}`).join('')
+  castToArray(document.get(xPath)!)[0].text(text)
+}
+
+// Mutates the document
+const setCdata = (document: Document, localXPath: string[], text: string): void => {
+  const xPath = localXPath.map(x => `/${nsTag(x)}`).join('')
+  ;(castToArray(document.get(xPath)!)[0] as any).cdata(text)
+}
+
 export const fillOpoForm = (data: OpoData): Document => {
   const opoTemplateContents = fs.readFileSync(path.join(__dirname, 'opo-template.xml'), {encoding: 'utf8'})
   const document = libxml.parseXmlString(opoTemplateContents)
-  castToArray(
-    document.get(`/${nsTag('PodaciPoreskeDeklaracije')}/${nsTag('PodaciOPrijavi')}/${nsTag('ObracunskiPeriod')}`)
-  )[0]!.text(data.dividendIncomeInfo.paymentDate.format('YYYY-MM'))
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPrijavi', 'ObracunskiPeriod'], data.dividendIncomeInfo.paymentDate.format('YYYY-MM'))
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPrijavi', 'DatumOstvarivanjaPrihoda'], data.dividendIncomeInfo.paymentDate.format('YYYY-MM-DD'))
+  // TODO: setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPrijavi', 'DatumDospelostiObaveze'], data.dividendIncomeInfo.paymentDate.format('YYYY-MM-DD'))
+  
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'PoreskiIdentifikacioniBroj'], data.jmbg)
+  setCdata(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'ImePrezimeObveznika'], data.fullName)
+  setCdata(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'UlicaBrojPoreskogObveznika'], data.streetAddress)
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'PrebivalisteOpstina'], data.opstinaCode)
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'JMBGPodnosiocaPrijave'], data.filerJmbg)
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'TelefonKontaktOsobe'], data.phone)
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciOPoreskomObvezniku', 'ElektronskaPosta'], data.email)
+  
+  setText(document, ['PodaciPoreskeDeklaracije', 'PodaciONacinuOstvarivanjaPrihoda', 'Ostalo'], data.realizationMethod)
+  // TODO: setText(document, ['PodaciPoreskeDeklaracije', 'DeklarisaniPodaciOVrstamaPrihoda', 'PodaciOVrstamaPrihoda', 'BrutoPrihod'], data.dividendIncomeInfo.grossDividend)
+
   return document
 }
--- tests/eporezi.test.ts
+++ tests/eporezi.test.ts
@@ -1,13 +1,28 @@
 import { fillOpoForm } from '../src/eporezi'
 import { toNaiveDate } from '../src/dates'
+import { fromCurrency } from '../src/rsd-amount'
 
 describe('fillOpoForm', () => {
   it('basic', () => {
     const result = fillOpoForm({
+      fullName: 'Jован Jовановић',
+      filingDeadline: toNaiveDate('2020-08-25'),
+      streetAddress: 'Terazije 1/1',
+      phone: '0611111111',
+      email: 'jovan@example.com',
+      opstinaCode: '016',
+      jmbg: '1234567890123',
+      filerJmbg: '1234567890123',
+      realizationMethod: 'Isplata na brokerski racun',
       dividendIncomeInfo: {
+        payingEntity: 'BMW',
+        grossDividend: fromCurrency(117, 100),
+        grossTaxPayable: fromCurrency(117, 15),
+        taxPaidAbroad: fromCurrency(117, 26),
+        taxPayable: fromCurrency(117, 0),
         paymentDate: toNaiveDate('2020-07-16'),
       },
-    } as any)
+    })
     console.log(result.toString())
   })
 })
