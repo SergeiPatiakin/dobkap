a7f54df92fb23e9c67325d6fff08ed7f0700d68e
Sergei Patiakin <sergei.patiakin@gmail.com>
2020-07-28 19:53:05 +0200

    Add disk cache for currency service

--- src/currencies/index.ts
+++ src/currencies/index.ts
@@ -1,8 +1,20 @@
 import { NaiveDate, CurrencyCode } from "../data-types"
 import { nbsCurrencyService } from "./nbs"
+import { formatNaiveDate } from "../dates"
+const Cache = require('async-disk-cache')
+
+const cache = new Cache('DobkapCurrencyService')
 
 export const currencyService = async (day: NaiveDate, currencyCode: CurrencyCode): Promise<number> => {
-  return nbsCurrencyService(day, currencyCode)
+  const cacheKey = `${formatNaiveDate(day)}-${currencyCode}`
+  if (await cache.has(cacheKey)){
+    const { value } = await cache.get(cacheKey)
+    return value
+  } else {
+    const value = await nbsCurrencyService(day, currencyCode)
+    await cache.set(cacheKey, value)
+    return value
+  }
 }
 
 export type CurrencyService = typeof currencyService
