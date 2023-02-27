a712577dfd928e86ceb92df97da07d49680d57a7
Sergei Patiakin <sergei.patiakin@gmail.com>
2023-02-27 18:31:44 +0100

    More unit tests

--- tests/ibkr-importer.test.ts
+++ tests/ibkr-importer.test.ts
@@ -33,6 +33,15 @@ describe('ibkrImporter', () => {
   it('basic - multiple dividends', async () => {
     const r = await ibkrImporter(path.join(__dirname, 'data/ibkr-section2.csv'))
     expect(r).toMatchObject([
+      // One dividend from ABC in EUR
+      {
+        incomeCurrencyCode: 'EUR',
+        payingEntity: 'ABC',
+        incomeCurrencyAmount: 60,
+        whtCurrencyCode: 'EUR',
+        whtCurrencyAmount: 0,
+      },
+      // One dividend from DEF1 in GBP
       {
         incomeCurrencyCode: 'GBP',
         payingEntity: 'DEF1',
@@ -40,15 +49,17 @@ describe('ibkrImporter', () => {
         whtCurrencyCode: 'GBP',
         whtCurrencyAmount: 0,
       },
+      // Two dividends from DEF2 in GBP, merged together
       {
         incomeCurrencyCode: 'GBP',
         payingEntity: 'DEF2',
-        incomeCurrencyAmount: 143,
+        incomeCurrencyAmount: 143, // Sum of amounts: 71 + 72
         whtCurrencyCode: 'GBP',
         whtCurrencyAmount: 0,
       },
     ])
     expect(formatNaiveDate(r[0].incomeDate)).toBe('2023-01-12')
     expect(formatNaiveDate(r[1].incomeDate)).toBe('2023-01-12')
+    expect(formatNaiveDate(r[2].incomeDate)).toBe('2023-01-12')
   })
 })
