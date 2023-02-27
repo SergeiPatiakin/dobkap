b708f8ab9f79c81c5a258ea7010f5a908fe9f253
Sergei Patiakin <sergei.patiakin@gmail.com>
2023-02-27 08:56:01 +0100

    Full CSV and section CSV tests

--- tests/ibkr-importer.test.ts
+++ tests/ibkr-importer.test.ts
@@ -4,8 +4,21 @@ import { formatNaiveDate } from '../src/dates'
 import { ibkrImporter } from '../src/importers/ibkr'
 
 describe('ibkrImporter', () => {
-  it('basic', async () => {
-    const r = await ibkrImporter(path.join(__dirname, 'data/ibkr-sample1.csv'))
+  it('basic - full report', async () => {
+    const r = await ibkrImporter(path.join(__dirname, 'data/ibkr-full1.csv'))
+    expect(r).toMatchObject(      [
+      {
+        incomeCurrencyCode: 'EUR',
+        payingEntity: 'ABC',
+        incomeCurrencyAmount: 60,
+        whtCurrencyCode: 'EUR',
+        whtCurrencyAmount: 6
+      }
+    ])
+    expect(formatNaiveDate(r[0].incomeDate)).toBe('2023-01-12')
+  })
+  it('basic - dividends section', async () => {
+    const r = await ibkrImporter(path.join(__dirname, 'data/ibkr-section1.csv'))
     expect(r).toMatchObject(      [
       {
         incomeCurrencyCode: 'EUR',
