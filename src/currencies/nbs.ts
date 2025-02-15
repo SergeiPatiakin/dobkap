3941e3602e7cc4acaa92e82cca53b144292e941a
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-02-16 00:02:33 +0100

    Fix NBS parser

--- src/currencies/nbs.ts
+++ src/currencies/nbs.ts
@@ -1,6 +1,6 @@
 import got from 'got'
 import { NaiveDate, CurrencyCode } from '../data-types'
-import { XMLParser } from 'fast-xml-parser'
+import { JSDOM } from 'jsdom'
 
 const nbsCurrencyCodeMapping: Map<string, CurrencyCode> = new Map([
   ['EUR', CurrencyCode.EUR],
@@ -23,55 +23,30 @@ const nbsCurrencyCodeMapping: Map<string, CurrencyCode> = new Map([
 export const nbsSupportedCurrencies = [...nbsCurrencyCodeMapping.values()]
 
 export const nbsCurrencyService = async (day: NaiveDate, currencyCode: CurrencyCode) => {
-  const prelimResult = await got.get('https://nbs.rs/kursnaListaModul/naZeljeniDan.faces')
-
-  // TODO: can we just hard-code values for jsessionid and viewState?
-  const jessionidRegex = /jsessionid=([0-9A-F]+)/g // MUTABLE!!!
-  const jsessionid = jessionidRegex.exec(prelimResult.body)![1]
-
-  const viewStateRegex = /id="j_id1:javax.faces.ViewState:0" value="([-0-9A-F\:]+)"/g // MUTABLE!!!
-  const viewState = viewStateRegex.exec(prelimResult.body)![1]
-  
-  const form = {
-    'index': 'index',
-    'index:brKursneListe': '',
-    'index:yearInner': day.format('YYYY'),
-    'index:inputCalendar1': day.format('DD/MM/YYYY'),
-    'index:vrstaInner': '3',
-    'index:prikazInner': '3',
-    'index:buttonShow': '',
-    'javax.faces.ViewState': viewState,
-  }
-
-  const result = await got.post('https://nbs.rs/kursnaListaModul/naZeljeniDan.faces', {
-    headers: {
-      Cookie: `JSESSIONID=${jsessionid}`
-    },
-    form,
+  let url = `https://webappcenter.nbs.rs/WebApp/ExchangeRate/ExchangeRate?isSearchExecuted=true&Date=${day.format('DD.MM.YYYY')}&ExchangeRateListTypeID=3`
+  const result = await got.get(url, {
+    // headers: {
+    //   Cookie: `JSESSIONID=${jsessionid}`
+    // },
+    // form,
   })
 
-  const parser = new XMLParser({ ignoreAttributes: false, parseTagValue: false })
-  const document = parser.parse(result.body)
-  
-  const nbsExchangeRates = document['Exchange_Rates_List']['Item'].map((c: any) => ({
-    nbsCurrencyCode: c['Currency'],
-    scaleFactor: Number(c['Unit']),
-    scaledExchangeRate: Number(c['Middle_Rate']),
-  })) as Array<{
-    nbsCurrencyCode: string,
-    scaleFactor: number,
-    scaledExchangeRate: number,
-  }>
+  const dom = new JSDOM(result.body)
+  const y = dom.window.document.querySelectorAll("table tr")
+  const exchangeRates = Array.from(y).flatMap(ye => {
+    const nbsCurrencyCode = ye.children[0].innerHTML
+    if (nbsCurrencyCode.length > 1 && nbsCurrencyCode.length <= 5) { // Filter out header ETC
+      return [{
+        nbsCurrencyCode,
+        scaleFactor: Number(ye.children[3].innerHTML),
+        scaledExchangeRate: Number(ye.children[4].innerHTML.replace(',', '.')),
+      }]
+    } else {
+      return []
+    }
+  })
 
-  const exchangeRates = nbsExchangeRates.map(
-    ({nbsCurrencyCode, scaleFactor, scaledExchangeRate}) => ({
-      currencyCode: nbsCurrencyCodeMapping.get(nbsCurrencyCode),
-      exchangeRate: scaledExchangeRate / scaleFactor
-    })
-  ).filter(x => x.currencyCode)
-  // TODO: cache entries in exchangeRates
+  const exchangeRate = exchangeRates.find(x => x.nbsCurrencyCode === currencyCode)!
 
-  const exchangeRate = exchangeRates.find(x => x.currencyCode === currencyCode)!.exchangeRate
-
-  return exchangeRate
+  return exchangeRate.scaledExchangeRate / exchangeRate.scaleFactor
 }
