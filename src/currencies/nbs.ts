e0474bd0f69df195c8a09dbc9c0574910d3ff622
Sergei Patiakin <sergei.patiakin@gmail.com>
2025-02-16 10:00:31 +0100

    Fix NBS parser and expose trivial importer (#40)

--- src/currencies/nbs.ts
+++ src/currencies/nbs.ts
@@ -1,7 +1,6 @@
 import got from 'got'
 import { NaiveDate, CurrencyCode } from '../data-types'
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
+import { XMLParser } from 'fast-xml-parser'
 
 const nbsCurrencyCodeMapping: Map<string, CurrencyCode> = new Map([
   ['EUR', CurrencyCode.EUR],
@@ -32,22 +31,37 @@ export const nbsCurrencyService = async (day: NaiveDate, currencyCode: CurrencyC
     // form,
   })
 
-  const dom = new JSDOM(result.body)
-  const y = dom.window.document.querySelectorAll("table tr")
-  const exchangeRates = Array.from(y).flatMap((ye: any) => {
-    const nbsCurrencyCode = ye.children[0].innerHTML
-    if (nbsCurrencyCode.length > 1 && nbsCurrencyCode.length <= 5) { // Filter out header ETC
-      return [{
-        nbsCurrencyCode,
-        scaleFactor: Number(ye.children[3].innerHTML),
-        scaledExchangeRate: Number(ye.children[4].innerHTML.replace(',', '.')),
-      }]
-    } else {
-      return []
-    }
-  })
 
-  const exchangeRate = exchangeRates.find(x => x.nbsCurrencyCode === currencyCode)!
+  const matches = result.body.match(new RegExp("/WebApp/ExchangeRate/ExchangeRate/Download\\?ExchangeRateListID=(?:[0-9a-f\\-]{36})&exchangeRateListTypeID=3&ExchangeRateListTypeName=srednjiKurs&Format=xml"))
+  if (matches === null) {
+    throw Error('NBS XML URL not found')
+  }
 
-  return exchangeRate.scaledExchangeRate / exchangeRate.scaleFactor
+  const url2 = 'https://webappcenter.nbs.rs' + matches[0]
+  const result2 = await got.get(url2, {})
+
+  const parser = new XMLParser({ ignoreAttributes: false, parseTagValue: false })
+  const document = parser.parse(result2.body)
+
+  const nbsExchangeRates = document['Exchange_Rates_List']['item'].map((c: any) => ({
+    nbsCurrencyCode: c['Currency'],
+    scaleFactor: Number(c['Unit']),
+    scaledExchangeRate: Number(c['Middle_Rate']),
+  })) as Array<{
+    nbsCurrencyCode: string,
+    scaleFactor: number,
+    scaledExchangeRate: number,
+  }>
+
+  const exchangeRates = nbsExchangeRates.map(
+    ({nbsCurrencyCode, scaleFactor, scaledExchangeRate}) => ({
+      currencyCode: nbsCurrencyCodeMapping.get(nbsCurrencyCode),
+      exchangeRate: scaledExchangeRate / scaleFactor
+    })
+  ).filter(x => x.currencyCode)
+  // TODO: cache entries in exchangeRates
+
+  const exchangeRate = exchangeRates.find(x => x.currencyCode === currencyCode)!.exchangeRate
+
+  return exchangeRate
 }
